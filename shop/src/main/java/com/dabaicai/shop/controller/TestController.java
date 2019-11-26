4fe45439d3b7cf8150d40f2105cb8dfe5f1666d4
廖宁波 <liaoningbo@aliyun.com>
2019-11-26 22:00:30 +0800

    集成Swagger接口文档

--- shop/src/main/java/com/dabaicai/shop/controller/TestController.java
+++ shop/src/main/java/com/dabaicai/shop/controller/TestController.java
@@ -1,14 +1,18 @@
-package com.dabaicai.shop;
+package com.dabaicai.shop.controller;
 
 import com.dabaicai.common.export.Export;
 import com.dabaicai.common.export.ExportRqeuest;
 import com.dabaicai.common.response.BasicResponse;
+import com.dabaicai.shop.TestService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 
+@Api(tags = "TestController", description = "测试")
 @RestController
 public class TestController {
     @Autowired
@@ -16,12 +20,13 @@ public class TestController {
     @Autowired
     private Export export;
 
-    @RequestMapping("/test")
+    @ApiOperation("测试")
+    @PostMapping("/test")
     public Object test(){
         return testService.test();
     }
 
-    @RequestMapping("/test1")
+    @PostMapping("/test1")
     public void test1(HttpServletResponse response){
         ExportRqeuest exportRqeuest = new ExportRqeuest();
         exportRqeuest.setSheetName("测试1");
